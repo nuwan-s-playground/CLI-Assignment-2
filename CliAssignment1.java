85c80c31f58ffd224c6268e7d1f40f04caf286eb
nuwan saranath <saranathnuwan@gmail.com>
2023-08-17 00:09:10 +0530

    Complete Code Using 2D Arrays

---
+++ CliAssignment1.java
@@ -0,0 +1,180 @@
+import java.util.Arrays;
+import java.util.Scanner;;
+
+public class CliAssignment1 {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        final String CLEAR = "\033[H\033[2J";
+        final String LIGHT_BLUE_COLOR_BOLD = "\033[36;1m";
+        final String PURPLE_COLOR_BOLD = "\033[35;1m";
+        final String GREEN_COLOR_BOLD = "\033[32;1m";
+        final String WHITE_COLOR_BOLD = "\033[37;1m";
+        final String BLUE_COLOR_BOLD = "\033[34;1m";
+        final String YELLOW_COLOR_BOLD = "\033[33;1m";
+        final String RED_COLOR_BOLD = "\033[31;1m";
+
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "Smart Banking System";
+        final String CREATE_ACCOUNT = "Create New Account";
+        final String DEPOSITE = "Deposit";
+        final String WITHDRAWAL = "Withdrawal";
+        final String TRANSFER = "Transfer";
+        final String PRINT_STATEMENT = "Print Statement";
+        final String DELETE_ACOOUNT = "Delete Account";
+        String screen = DASHBOARD;
+        int[] idArray = new int[0];
+        double[] balanceArray = new double[0];
+        String[] nameArray = new String[0];
+        int idnum;
+        boolean valid = true;
+        boolean twoValidation = false;
+        String[][] details = new String[0][3];
+        String name;
+        double initialDepost;
+        String id;
+
+        do {
+            String customerID;
+            final String APP_TITLE = String.format("%s%s%s", GREEN_COLOR_BOLD, screen, RESET);
+
+            System.out.println(CLEAR);
+            System.out.printf("%s%s%s \n", GREEN_COLOR_BOLD, "-".repeat(100), RESET);
+            System.out.printf("%s%s%s \n", " ".repeat((100 - APP_TITLE.length()) / 2), APP_TITLE,
+                    " ".repeat((100 - APP_TITLE.length()) / 2));
+            System.out.printf("%s%s%s \n\n", GREEN_COLOR_BOLD, "-".repeat(100), RESET);
+            switch (screen) {
+                case DASHBOARD: {
+
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[1].Create New Account", RESET);
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[2].Deposit", RESET);
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[3].Withdrawal", RESET);
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[4].Transfer", RESET);
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[5].Print Statement", RESET);
+                    System.out.printf("%s%s%s \n", BLUE_COLOR_BOLD, "[6].Delete Account", RESET);
+                    System.out.printf("%s%s%s \n\n", BLUE_COLOR_BOLD, "[7].Exit", RESET);
+
+                    System.out.printf("%s%s%s%s", WHITE_COLOR_BOLD, "Enter an option to continue : ", RESET,LIGHT_BLUE_COLOR_BOLD);
+
+                    int option = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (option) {
+                        case 1: {
+                            screen = CREATE_ACCOUNT;
+                            break;
+                        }
+                        case 2: {
+                            screen = DEPOSITE;
+                            break;
+                        }
+                        case 3: {
+                            screen = WITHDRAWAL;
+                            break;
+                        }
+                        case 4: {
+                            screen = TRANSFER;
+                            break;
+                        }
+                        case 5: {
+                            screen = PRINT_STATEMENT;
+                            break;
+                        }
+                        case 6: {
+                            screen = DELETE_ACOOUNT;
+                            break;
+                        }
+                        case 7: {
+                            System.exit(0);
+                            break;
+                        }
+                        default:
+                            continue;
+
+                    }
+                    break;
+                }
+                case CREATE_ACCOUNT: {
+
+                    idnum = idArray.length + 1;
+                    valid = false;
+                    id = String.format("SDB-%05d", (details.length + 1));
+                    System.out.println(id);
+                    System.out.printf("%sID\t\t: %s%sSDB-%05d %s \n", WHITE_COLOR_BOLD, RESET, LIGHT_BLUE_COLOR_BOLD,
+                            (details.length + 1), RESET);
+
+                    loop: do {
+                        valid = false;
+
+                        System.out.printf("%sName\t\t: %s%s", WHITE_COLOR_BOLD, RESET, LIGHT_BLUE_COLOR_BOLD);
+                        name = scanner.nextLine().strip();
+                        if (name.isBlank()) {
+                            System.out.printf("%sName can't be empty%s\n", RED_COLOR_BOLD, RESET);
+                            valid = true;
+                            continue;
+                        }
+                        for (int index = 0; index < name.length(); index++) {
+                            if (!(Character.isLetter(name.charAt(index))
+                                    || Character.isSpaceChar(name.charAt(index)))) {
+                                System.out.printf("%sInvalid Name%s\n", RED_COLOR_BOLD, RESET);
+                                valid = true;
+                                continue loop;
+                            }
+                        }
+
+                    } while (valid);
+
+                    do {
+                        twoValidation = false;
+                        System.out.printf("%sInitial Deposit\t:%s%s ", WHITE_COLOR_BOLD, RESET, LIGHT_BLUE_COLOR_BOLD);
+                        initialDepost = scanner.nextDouble();
+                        scanner.nextLine();
+                        if (initialDepost < 5000) {
+                            System.out.printf("%sInsufficient Amount%s\n", RED_COLOR_BOLD, RESET);
+                            twoValidation = true;
+                            continue;
+                        }
+
+
+                    } while (twoValidation);
+                    String[][] newDetails = new String[details.length + 1][3];
+
+                    for (int i = 0; i < details.length; i++) {
+                        for (int index = 0; index < 3; index++) {
+                            newDetails[i][index] = details[i][index];
+                        }
+
+                    }
+
+                    newDetails[newDetails.length - 1][0] = id; 
+                    newDetails[newDetails.length-1][1] = name;
+                    newDetails[newDetails.length - 1][2] = String.valueOf(initialDepost);
+                    details = newDetails;
+                    System.out.println(Arrays.toString(details[details.length-1]));
+                    System.out.printf("\n%sID : %s%s Name : %s has been created successfully. %s\n\n",
+                            GREEN_COLOR_BOLD, "SDK-",
+                            id, details[details.length-1][2], RESET);
+
+                    System.out.printf("%sDo you want to add new student (Y/n)?  : %s%s ", WHITE_COLOR_BOLD, RESET,
+                            LIGHT_BLUE_COLOR_BOLD);
+                    String secondOption = scanner.nextLine().strip().toUpperCase();
+                    if (secondOption.equals("Y")) {
+                        screen = CREATE_ACCOUNT;
+                        valid = true;
+
+                    } else {
+                        screen = DASHBOARD;
+                        valid = true;
+                    }
+
+                }
+                
+
+
+            }
+
+        } while (valid);
+    }
+
+}
